5f11c7b6c98b6f3e7d33a786dcaa1df77ae4ffb4
Vincent Perry Divino <156486099+vpmdivino@users.noreply.github.com>
2025-09-24 23:52:30 +0800

    Add files via upload

---
+++ BlogDataLibrary/Data/ISqlData.cs
@@ -0,0 +1,12 @@
+﻿using BlogDataLibrary.Models;
+
+namespace BlogDataLibrary.Data
+{
+    public interface ISqlData
+    {
+        void AddPost(PostModel post);
+        UserModel Authenticate(string username, string password);
+        List<ListPostModel> ListPosts();
+        void Register(string username, string firstName, string lastName, string password);
+    }
+}
---
+++ BlogDataLibrary/Data/SqlData.cs
@@ -0,0 +1,56 @@
+﻿using BlogDataLibrary.Database;
+using BlogDataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlogDataLibrary.Data
+{
+    public class SqlData
+    {
+        private ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public UserModel Authenticate(string username, string password)
+        {
+            UserModel result = _db.LoadData<UserModel, dynamic>("dbo.spUsers_Authenticate",
+                                                                new { username, password },
+                                                                connectionStringName,
+                                                                true).FirstOrDefault();
+            return result;
+        }
+
+        public void Register(string username, string firstName, string lastName, string password)
+        {
+            _db.SaveData<dynamic>(
+                "dbo.spUsers_Register",
+                new { username, firstName, lastName, password },
+                connectionStringName,
+                true);
+        }
+
+        public void AddPost(PostModel post)
+        {
+            _db.SaveData("dbo.spPosts_Insert",
+                         new { post.UserID, post.Title, post.Body, post.DateCreated },
+                         connectionStringName,
+                         true);
+        }
+
+        public List<ListPostModel> ListPosts()
+        {
+            return _db.LoadData<ListPostModel, dynamic>("dbo.spPosts_List", new { }, connectionStringName, true).ToList();
+        }
+
+        public ListPostModel ShowPostDetails(int id)
+        {
+            return _db.LoadData<ListPostModel, dynamic>("dbo.spPosts_Details", new { id }, connectionStringName, true).FirstOrDefault();
+        }
+
+    }
+}
