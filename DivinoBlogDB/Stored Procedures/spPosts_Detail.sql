9fb2b1dc680594373822d309a86e33cd6bb90fa9
Vincent Perry Divino <156486099+vpmdivino@users.noreply.github.com>
2025-09-24 23:49:24 +0800

    Add files via upload

---
+++ DivinoBlogDB/Stored Procedures/spPosts_Detail.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_Details]
+    @id int
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        [p].[Id], 
+        [p].[Title], 
+        [p].[Body], 
+        [p].[DateCreated], 
+        [u].[UserName], 
+        [u].[FirstName], 
+        [u].[LastName]
+    FROM dbo.Posts p
+    INNER JOIN dbo.Users u
+        ON p.UserID = u.Id
+    WHERE p.Id = @id;
+END
---
+++ DivinoBlogDB/Stored Procedures/spPosts_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_Insert]
+    @userId int,
+    @title nvarchar(150),
+    @body text,
+    @dateCreated datetime2
+AS
+begin
+    set nocount on;
+    
+    INSERT INTO dbo.Posts (UserID, Title, Body, DateCreated)
+    VALUES (@userId, @title, @body, @dateCreated);
+end
